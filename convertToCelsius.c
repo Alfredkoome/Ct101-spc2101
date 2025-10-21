2ad16128f495dec3b5dc1ea94e152bfeb09c1a9d
Alfred koome <koome.26551@student.kyu.ac.ke>
2025-10-21 19:04:58 +0300

    convert fahrenheit into celsious

---
+++ convertToCelsius.c
@@ -0,0 +1,23 @@
+//simple programin
+/*
+Name:Alfred koome
+adm:CT101/g/26551/25
+Description:c program to calculate fare
+*/
+
+#include<stdio.h>
+
+int convertToCelsius(int f) {
+    int c;
+    c = (f - 32) * 5 / 9;
+    return c;
+}
+
+int main() {
+    int f;
+    printf("enter temperature in fahrenheit\n");
+    scanf("%d", &f);
+    int c = convertToCelsius(f);
+    printf("temperature into celsius: %d", c);
+    return 0;
+}
